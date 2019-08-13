293e83c29c6ab417cdbe7509a28b26dfdff67a8b
inmount <inmount@163.com>
2019-08-13 11:27:52 +0800

    1.1.1908.12
    将Orm示例移出标准代码库
    强化行操作器功能应用
    增加对PostgreSql数据库的支持

---
+++ RowOperator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dpz2.db {
+
+    /// <summary>
+    /// 行数据操作器
+    /// </summary>
+    public abstract class RowOperator : dpz2.Object {
+
+        /// <summary>
+        /// 获取行数据对象
+        /// </summary>
+        protected Row Row { get; private set; }
+
+        /// <summary>
+        /// 对象实例化
+        /// </summary>
+        /// <param name="row"></param>
+        public RowOperator(Row row) {
+            this.Row = row;
+        }
+
+        /// <summary>
+        /// 获取基础数据行对象
+        /// </summary>
+        /// <returns></returns>
+        public Row GetRow() { return this.Row; }
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        protected override void OnDispose() {
+            base.OnDispose();
+
+            this.Row = null;
+        }
+
+    }
+}
